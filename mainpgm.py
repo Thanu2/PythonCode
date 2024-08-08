ef1b93d96a6a7c687289425c2cf1ad2116d80863
Bhavya <bhavya.murthy4@gmail.com>
2024-08-08 10:41:54 +0530

    Adding new build

---
+++ mainpgm.py
@@ -0,0 +1,5 @@
+def mainpgm():
+	print("Im in main")
+    
+def maintwo():
+    print("Im in second function of main")
---
+++ sub/subpgm.py
@@ -0,0 +1,5 @@
+def subpgm():
+	print("Im in sub")
+    
+def subtwo():
+    print("Im in second function in sub")
